794739db25aa8ecadbc4410494965d3777b8cdd8
EC2 Default User <ec2-user@ip-172-31-4-178.ap-northeast-1.compute.internal>
2022-12-27 13:39:49 +0000

    finish

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,59 @@
+class BooksController < ApplicationController
+  before_action :matching_id, only: [:edit]
+
+  def show
+    @post_book = Book.find(params[:id])
+    @book = Book.new
+    @user = User.find(@post_book.user_id)
+  end
+
+  def index
+    @book = Book.new
+    @books = Book.all
+  end
+
+  def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    if @book.save
+      redirect_to book_path(@book.id), notice: "You have created book successfully."
+    else
+      @books = Book.all
+      render 'index'
+    end
+  end
+
+  def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      redirect_to book_path(@book), notice: "You have updated book successfully."
+    else
+      render "edit"
+    end
+  end
+
+  def destroy
+    @book = Book.find(params[:id])
+    @book.destroy
+    redirect_to books_path
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+  
+  def matching_id
+    @book = Book.find(params[:id])
+    user_id = current_user.id
+    book_id = @book.user.id
+    unless user_id == book_id
+      redirect_to books_path
+    end
+  end
+end

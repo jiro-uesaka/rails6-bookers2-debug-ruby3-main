fe30a1f3becb064019eb6430b0b761bb8744101b
EC2 Default User <ec2-user@ip-172-31-4-178.ap-northeast-1.compute.internal>
2023-01-02 06:33:55 +0000

    1/2

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   devise_for :users
   root to: "homes#top"
@@ -14,5 +13,6 @@ Rails.application.routes.draw do
     resources :book_comments, only: [:create, :destroy]
     resource :favorites, only: [:create, :destroy]
   end
+  get "search" => "searches#search", as: "search"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
